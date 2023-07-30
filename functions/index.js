b2dbeb1312bc277148eb2eedc20b04558467d422
Omri Kalman <omrkalman@gmail.com>
2023-07-30 23:24:02 +0300

    backend started and deployed

--- functions/index.js
+++ functions/index.js
@@ -1,47 +1,54 @@
-const { initializeApp } = require('firebase-admin/app');
-const admin = require('firebase-admin');
+// functions/index.js
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const express = require('express');
+const app = express();
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDedcqc3kajuCE9HJjisUzryz8lhKLs874",
-    authDomain: "va-ad-bayit.firebaseapp.com",
-    projectId: "va-ad-bayit",
-    storageBucket: "va-ad-bayit.appspot.com",
-    messagingSenderId: "678454354829",
-    appId: "1:678454354829:web:75b9c144213ce3d99553c4",
-    measurementId: "G-GVELKG9CWB"
-};
-
-const app = initializeApp(firebaseConfig);
+// Initialize Firebase Admin SDK
 admin.initializeApp();
 
 const firestore = admin.firestore();
 
-exports.protectedData = functions.https.onRequest(async (req, res) => {
-    const accessToken = req.headers.authorization;
+// Middleware to validate the access token
+const validateAccessToken = async (req, res, next) => {
+  const accessToken = req.headers.authorization;
 
-    if (!accessToken || !accessToken.startsWith('Bearer ')) {
-        return res.status(401).json({ error: 'Unauthorized access.' });
-    }
+  if (!accessToken || !accessToken.startsWith('Bearer ')) {
+    return res.status(401).json({ error: 'Unauthorized access.' });
+  }
 
-    const idToken = accessToken.split('Bearer ')[1];
+  const idToken = accessToken.split('Bearer ')[1];
 
-    try {
-        // Verify the provided access token using Firebase Admin SDK
-        const decodedToken = await admin.auth().verifyIdToken(idToken);
-        const userId = decodedToken.uid; // Get the user's UID from the decoded token
+  try {
+    // Verify the provided access token using Firebase Admin SDK
+    const decodedToken = await admin.auth().verifyIdToken(idToken);
+    req.user = decodedToken; // Save the decoded token in the request object
+    next();
+  } catch (error) {
+    console.error('Error verifying access token:', error);
+    return res.status(401).json({ error: 'Unauthorized access.' });
+  }
+};
 
-        // Implement your custom access control logic here
-        // For example, check if the user has permission to access the protected document
-        const userDoc = await firestore.collection('users').doc(userId).get();
-        if (!userDoc.exists) {
-            return res.status(403).json({ error: 'User not authorized to access the protected data.' });
-        }
+// Protected Data route
+app.get('/binyan', validateAccessToken, async (req, res) => {
+  const userId = req.user.uid; // Get the user's UID from the decoded token
 
-        // Return the protected data to the user
-        return res.json({ data: 'This is the protected data.' });
-    } catch (error) {
-        console.error('Error verifying access token:', error);
-        return res.status(401).json({ error: 'Unauthorized access.' });
-    }
+  try {
+    // Implement your custom access control logic here
+    // For example, check if the user has permission to access the protected document
+    // const userDoc = await firestore.collection('binyan').doc(userId).get();
+    // if (!userDoc.exists) {
+    //   return res.status(403).json({ error: 'User not authorized to access the protected data.' });
+    // }
+
+    // Return the protected data to the user
+    return res.json({ data: 'This is the protected data.' });
+  } catch (error) {
+    console.error('Error accessing Firestore:', error);
+    return res.status(500).json({ error: 'Error accessing Firestore.' });
+  }
 });
+
+// Connect the Express app to the Cloud Function
+exports.api = functions.https.onRequest(app);
